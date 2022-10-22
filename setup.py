d084426795916a560aec3aa490ea1055c364bebd
jbungenerflex <jim@flexcompute.com>
2022-10-22 09:16:45 +0200

    added setup.py from maciej branch

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+      name='Flow360Scripts',
+      version='1.0',
+      description='Scripts for Flow360',
+      author='Flexcompute Inc.',
+      author_email='support@flexcompute.com',
+      url='www.flexcompute.com',
+      packages=['preprocessing', 'preprocessing.BETDisk'],
+      python_requires=">=3.6",
+)
