3c36d9f54a4bb65cea9c5c2e0a5ab04f2e1166d1
cjdoolittle <cj@flexcompute.com>
2023-09-06 22:15:31 -0700

    refactor Convergence_Plots.py

--- examples/Solver_Features/Wall_Model/Convergence_Plots.py
+++ examples/Solver_Features/Wall_Model/Convergence_Plots.py
@@ -4,11 +4,11 @@ import os, csv
 import numpy as np
 import pandas as pd
 
-
 with open('caseNameList.dat', 'r') as file:
     caseNameList = file.read().splitlines()
 
-naming = ["Wall Resolved", "Wall Modeled"]
+loads = ['CL', 'CD']
+residuals = ['0_cont', '1_momx', '2_momy', '3_momz', '4_energ', '5_nuHat']
 figures = []
 axes = []
 
@@ -22,26 +22,23 @@ def computeCLCD(data):
 
 def plotLoads(data, plotName):
     x = data['pseudo_step']
-    keys = ['CL', 'CD']
-    for ax, key in zip(axes, keys):
-        ax.plot(x, data[key], label=plotName)
-        ax.set_ylabel(key)
+    for ax, load in zip(axes, loads):
+        ax.plot(x, data[load], label=plotName)
+        ax.set_ylabel(load)
         ax.legend()
         ax.set_xlabel('Pseudo step')
         ax.grid(which='major', color='gray')
-        if key == 'CL':
+        if load == 'CL':
             ax.set_ylim(-0.2, -0.1)
-        elif key == 'CD':
+        elif load == 'CD':
             ax.set_ylim(0.25, 0.45)
 
 
-def plotResiduals(res, plotName):
-    x = res['pseudo_step']
-    keys = ['0_cont', '1_momx', '2_momy', '3_momz', '4_energ', '5_nuHat']
-    labels = ['Cont Residual', 'Momx Residual', 'Momy Residual', 'Momz Residual', 'Energy Residual', 'nuHat Residual']
-    for ax, key, label in zip(axes, keys, labels):
-        ax.semilogy(x, res[key], label=plotName)
-        ax.set_ylabel(label)
+def plotResiduals(data, plotName):
+    x = data['pseudo_step']
+    for ax, res in zip(axes, residuals):
+        ax.semilogy(x, data[res], label=plotName)
+        ax.set_ylabel(res)
         ax.legend()
         ax.grid(which='major', color='gray')
         ax.set_xlabel('Pseudo step')
@@ -51,45 +48,42 @@ def plotResiduals(res, plotName):
 
 
 def main():
-    # plot loads convergence histories
-    for i in [0, 1]:
+    # set output directory
+    dir_path = os.path.join(os.getcwd(), 'figures')
+    os.makedirs(dir_path, exist_ok=True)
+
+    # initialize figures & axes
+    for load in loads:
         fig, ax = plt.subplots(figsize=(8, 6))
         figures.append(fig)
         axes.append(ax)
-    dir_path = os.path.join(os.getcwd(), f'figures')
-    if not os.path.isdir(dir_path):
-        os.makedirs(dir_path)
 
-    for j, resolution in enumerate(caseNameList):
-        csvPath = os.path.join(os.getcwd(), f'{resolution}', 'surface_forces_v2.csv')
+    # calculate and plot loads convergence histories
+    for caseName in caseNameList:
+        csvPath = os.path.join(os.getcwd(), caseName, 'surface_forces_v2.csv')
         data = pd.read_csv(csvPath, skipinitialspace=True)
         computeCLCD(data)
-        plotLoads(data, naming[j])
+        plotLoads(data, caseName)
 
-    figures[0].savefig(f'figures/CL.png', dpi=500)
-    figures[1].savefig(f'figures/CD.png', dpi=500)
+    # save loads figures
+    for i, load in enumerate(loads):
+        figures[i].savefig(os.path.join(dir_path, load + '.png'), dpi=500)
 
     for ax in axes:
         ax.cla()
 
     # plot residual convergence histories
-    for i in range(0, 6):
+    for res in residuals:
         fig, ax = plt.subplots(figsize=(8, 6))
         figures.append(fig)
         axes.append(ax)
-    for j, resolution in enumerate(caseNameList):
-        csvPath = os.path.join(os.getcwd(), f'{resolution}', 'nonlinear_residual_v2.csv')
-        res = pd.read_csv(csvPath, skipinitialspace=True)
-        plotResiduals(res, naming[j])
-
-    figures[0].savefig(f'figures/Cont.png', dpi=500)
-    figures[1].savefig(f'figures/Momx.png', dpi=500)
-    figures[2].savefig(f'figures/Momy.png', dpi=500)
-    figures[3].savefig(f'figures/Momz.png', dpi=500)
-    figures[4].savefig(f'figures/Energy.png', dpi=500)
-    figures[5].savefig(f'figures/nuHat.png', dpi=500)
+    for caseName in caseNameList:
+        csvPath = os.path.join(os.getcwd(), caseName, 'nonlinear_residual_v2.csv')
+        data = pd.read_csv(csvPath, skipinitialspace=True)
+        plotResiduals(data, caseName)
+    for i, res in enumerate(residuals):
+        figures[i].savefig(os.path.join(dir_path, res + '.png'), dpi=500)
 
 
 if __name__ == '__main__':
     main()
-
